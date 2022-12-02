7abc5c564801c01b3be12464c68f49e1f5aae57f
Diego Vazquez <diegovazquezok@gmail.com>
2022-12-02 18:58:33 -0300

    Cambios en error de Leer Stock

--- AppLibreria/admin.py
+++ AppLibreria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from AppLibreria.models import *
+
+
 
 # Register your models here.
+
+admin.site.register(Libro)
+admin.site.register(Proveedores)
+admin.site.register(Cliente)
